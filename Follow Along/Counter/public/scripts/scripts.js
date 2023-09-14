da11ad96088818de70fccc2f5908bd0852a673e3
scanunicco <Scanuproductions@gmail.com>
2023-09-14 14:45:22 -0400

    Counter

---
+++ Follow Along/Counter/public/scripts/scripts.js
@@ -0,0 +1,43 @@
+var countNum = 0;
+function countUp() {
+    countNum++;
+    displayCount();
+}
+
+function countDown() {
+    countNum--;
+    displayCount();
+}
+
+function countZero() {
+    countNum = 0;
+    displayCount();
+}
+
+function displayCount() {
+    document.getElementById("display").innerText = countNum;
+}
+
+document.querySelector("#dec").onclick = countDown;
+document.querySelector("#zero").onclick = countZero;
+document.querySelector("#inc").onclick = countUp;
+document.body.addEventListener("keydown", function(e) {
+    console.log(e.key);
+    if(e.key == "ArrowDown") {
+        countDown();
+    } else if (e.key == "ArrowUp") {
+        countUp();
+    } else if (e.key == "0") {
+        countZero();
+    } else if (e.key == "ArrowLeft") {
+        if(countNum % 10 == 0){
+            countNum *= .1;
+            displayCount();
+        }
+    } else if (e.key == "ArrowRight") {
+        if(("" + countNum).length < 7){
+            countNum *= 10;
+            displayCount();
+        }
+    }
+})
